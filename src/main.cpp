2dc0601c2e02a96529062336bb131f849aefacec
Felix Lipski <felix.lipski7@gmail.com>
2023-09-03 12:35:39 +0200

    some fps movement

--- src/main.cpp
+++ src/main.cpp
@@ -16,29 +16,46 @@ struct Cube {
 
 struct Player {
     btRigidBody* capsule;
+    float height;
+    float radius;
+    float innerHeight;
 };
 
-float height = 1.8;
-float radius = 0.25;
-float innerHeight = height - radius - radius;
+struct PlayerHead {
+    raylib::Camera* camera;
+};
+
 
 flecs::entity initPlayer(flecs::world& ecs, btVector3 position) {
+    float height = 1.8;
+    float radius = 0.25;
+    float innerHeight = height - radius - radius;
+    btVector3 centerPosition = position + btVector3(0, height/2, 0);
+
     btCollisionShape* collisionShape = new btCapsuleShape(btScalar(radius), btScalar(innerHeight));
     btTransform transform;
     transform.setIdentity();
-    transform.setOrigin(position);
+    transform.setOrigin(centerPosition);
     btScalar mass(1.);
     btVector3 localInertia(0, 0, 0);
     collisionShape->calculateLocalInertia(mass, localInertia);
     btDefaultMotionState* myMotionState = new btDefaultMotionState(transform);
     btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, collisionShape, localInertia);
     btRigidBody* body = new btRigidBody(rbInfo);
+    btVector3 angularFactor(0, 1, 0);
+    body->setAngularFactor(angularFactor);
     ecs.get<PhysicsWorld>()->dynamicsWorld->addRigidBody(body);
-    return ecs.entity().set(Player{body});
+
+    /* raylib::Camera camera({ 10.0f, 2.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f); */
+    /* raylib::Camera *camera = new raylib::Camera({ 10.0f, 2.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f); */
+    raylib::Camera *camera = new raylib::Camera({ position.x(), position.y() + (height - radius), position.z() }, { position.x(), position.y() + (height - radius), position.z() + 2.0f}, { 0.0f, 1.0f, 0.0f }, 45.0f);
+    /* ecs.entity().set(PlayerHead{&camera}); */
+
+    return ecs.entity()
+        .set(Player{body, height, radius, innerHeight})
+        .set(PlayerHead{camera}) ;
 }
-void renderPlayer() {
-    DrawCapsuleWires((Vector3){0.0f, radius, 0.0f}, (Vector3){0.0f, radius + innerHeight, 0.0f}, radius, 8, 8, PURPLE);
-}
+
 /* DrawCapsuleWires((Vector3){-3.0f, 1.5f, -4.0f}, (Vector3){-4.0f, -1.0f, -4.0f}, 1.2f, 8, 8, PURPLE); */
 
 void renderCube(btRigidBody* body, Model model) {
@@ -87,25 +104,77 @@ flecs::entity createCube(flecs::world& ecs, btVector3 position) {
 
 int main(int, char *[]) {
     raylib::Color textColor = raylib::Color::LightGray();
-    raylib::Window window(1920, 1080, "raylib [core] example - basic window");
+    raylib::Window window(1000, 500, "raylib [core] example - basic window");
+    /* raylib::Window window(1920, 1080, "raylib [core] example - basic window"); */
     SetTargetFPS(60);
     /* ToggleFullscreen(); */
-    raylib::Camera camera({ 10.0f, 2.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f);
     flecs::world ecs;
 
     btDiscreteDynamicsWorld* dynamicsWorld = initBullet();
     ecs.set(PhysicsWorld{dynamicsWorld});
 
-    ecs.system("BeforeDraw")
-        .kind(flecs::PreUpdate)
-        .iter([&](flecs::iter& it) {
-            /* printf("Time: %f\n", it.delta_time()); */
-		    dynamicsWorld->stepSimulation(1.f / 60.f, 10);
+    /* raylib::Camera camera({ 10.0f, 2.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f); */
+    /* ecs.entity().set(PlayerHead{&camera}); */
 
-            /* camera.Update(CAMERA_FIRST_PERSON); */
+    DisableCursor();
+
+    /* ecs.system<PlayerHead>("UpdateHead") */
+    /*     .kind(flecs::OnUpdate) */
+    /*     .each([](PlayerHead& ph) { */
+    /*         if (IsKeyDown(KEY_W)) { */
+    /*             ph.camera->position.x += 0.1; */
+    /*         } */
+    /*         /1* ph.camera->position.x += 0.1; *1/ */
+    /*         /1* ph.camera->Update(CAMERA_FIRST_PERSON); *1/ */
+    /*         /1* ph.camera->Update(CAMERA_FREE); *1/ */
+    /*     }); */
+
+    ecs.system<Player, PlayerHead>("Movement")
+        .kind(flecs::OnUpdate)
+        .each([](Player& player, PlayerHead& ph) {
+            /* btTransform = player.getCenterOfMassTransform(); */
+            btVector3 centerPos = player.capsule->getCenterOfMassPosition();
+            ph.camera->position.x = centerPos.x();
+            ph.camera->position.y = centerPos.y() + (player.innerHeight/2);
+            ph.camera->position.z = centerPos.z();
+
+            ph.camera->target.x = centerPos.x();
+            ph.camera->target.y = centerPos.y() + (player.innerHeight/2);
+            ph.camera->target.z = centerPos.z() + 2.0;
+
+            /* printf("%f\n",ph.camera->position.y); */
+
+            /* forward */
+            if (IsKeyDown(KEY_E)) {
+                player.capsule->applyCentralForce(btVector3(btScalar(0.), btScalar(0.), btScalar(10.)));
+            }
+            /* backward */
+            if (IsKeyDown(KEY_D)) {
+                player.capsule->applyCentralForce(btVector3(btScalar(0.), btScalar(0.), btScalar(-10.)));
+            }
+            /* right */
+            if (IsKeyDown(KEY_S)) {
+                player.capsule->applyCentralForce(btVector3(btScalar(10.), btScalar(0.), btScalar(0.)));
+            }
+            /* left */
+            if (IsKeyDown(KEY_F)) {
+                player.capsule->applyCentralForce(btVector3(btScalar(-10.), btScalar(0.), btScalar(0.)));
+            }
+        });
+
+    ecs.system<PhysicsWorld>("StepPhysics")
+        .kind(flecs::PreUpdate)
+        .each([](PhysicsWorld& pw) {
+            pw.dynamicsWorld->stepSimulation(1.f / 60.f, 10);
+        });
+
+    ecs.system<PlayerHead>("BeginDrawing")
+        .kind(flecs::PreUpdate)
+        .each([](PlayerHead& ph) {
             BeginDrawing();
             ClearBackground(DARKBLUE);
-            BeginMode3D(camera);
+            /* ph.camera->Update(CAMERA_FIRST_PERSON); */
+            ph.camera->BeginMode();
             DrawGrid(100, 1.0f);
         });
 
@@ -118,13 +187,18 @@ int main(int, char *[]) {
     ecs.system<Player>("DrawPlayer")
         .kind(flecs::OnUpdate)
         .each([](Player& p) {
-            renderPlayer();
-            /* renderCube(c.body, c.model); */
+            /* renderPlayer(); */
+            btVector3 centerPos = p.capsule->getCenterOfMassPosition();
+            DrawCapsuleWires(
+                    (Vector3){centerPos.x(), centerPos.y() - (p.innerHeight/2), centerPos.z()},
+                    (Vector3){centerPos.x(), centerPos.y() + (p.innerHeight/2), centerPos.z()},
+                    p.radius, 8, 8, PURPLE);
+            /* DrawCapsuleWires((Vector3){0.0f, p.radius, 0.0f}, (Vector3){0.0f, p.radius + p.innerHeight, 0.0f}, p.radius, 8, 8, GREEN); */
         });
 
     ecs.system("AfterDraw")
         .kind(flecs::PostUpdate)
-        .iter([&](flecs::iter& it) {
+        .iter([](flecs::iter& it) {
             EndMode3D();
             EndDrawing();
         });
@@ -132,7 +206,7 @@ int main(int, char *[]) {
 
     {
         // TODO free shapes automatically
-        btCollisionShape* groundShape = new btBoxShape(btVector3(btScalar(10.), btScalar(10.), btScalar(10.)));
+        btCollisionShape* groundShape = new btBoxShape(btVector3(btScalar(100.), btScalar(10.), btScalar(100.)));
         btTransform groundTransform;
         groundTransform.setIdentity();
         groundTransform.setOrigin(btVector3(0, -10, 0));
@@ -149,11 +223,11 @@ int main(int, char *[]) {
         flecs::entity cube1 = ecs.entity("Ground").set(Cube{body, model});
     }
 
-    /* createCube(ecs, btVector3(1, 15, 1)); */
-    /* createCube(ecs, btVector3(-1, 17, 0)); */
-    /* createCube(ecs, btVector3(-2, 1, 0)); */
+    createCube(ecs, btVector3(1, 15, 1));
+    createCube(ecs, btVector3(-1, 17, 0));
+    createCube(ecs, btVector3(-2, 1, 0));
     createCube(ecs, btVector3(0, 6, 0));
-    initPlayer(ecs, btVector3(0, 2, 0));
+    initPlayer(ecs, btVector3(0, 0, 0));
 
     while (!window.ShouldClose()) {
     	ecs.progress();
