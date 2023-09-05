abeabd350bc1c7c93bf6f7a5f5b5d98ac5b85952
Felix Lipski <felix.lipski7@gmail.com>
2023-09-05 22:17:41 +0200

    put all junk into Fizyk class

--- src/foo.cpp
+++ src/foo.cpp
@@ -207,16 +207,18 @@ class Fizyk {
 private:
 	TempAllocatorImpl temp_allocator;
 	JobSystemThreadPool job_system;
+	MyBodyActivationListener body_activation_listener;
+	MyContactListener contact_listener;
 public:
 	PhysicsSystem physics_system;
     Fizyk() : temp_allocator(10 * 1024 * 1024), job_system(cMaxPhysicsJobs, cMaxPhysicsBarriers, thread::hardware_concurrency() - 1) {
-	    /* TempAllocatorImpl temp_allocator(10 * 1024 * 1024); */
-	    /* JobSystemThreadPool job_system(cMaxPhysicsJobs, cMaxPhysicsBarriers, thread::hardware_concurrency() - 1); */
-	    /* this->temp_allocator = TempAllocatorImpl(10 * 1024 * 1024); */
-	    /* JobSystemThreadPool job_system(cMaxPhysicsJobs, cMaxPhysicsBarriers, thread::hardware_concurrency() - 1); */
-        /* this->temp_allocator = TempAllocator(); // Initialize TempAllocator */
-        /* this->job_system = JobSystem();         // Initialize JobSystem */
-	    /* PhysicsSystem * physics_system = new PhysicsSystem(); */
+	    /* RegisterDefaultAllocator(); */
+	    Trace = TraceImpl;
+	    JPH_IF_ENABLE_ASSERTS(AssertFailed = AssertFailedImpl;)
+
+	    Factory::sInstance = new Factory();
+
+	    RegisterTypes();
 
 	    const uint cMaxBodies = 1024;
 	    const uint cNumBodyMutexes = 0;
@@ -227,155 +229,60 @@ public:
 	    ObjectLayerPairFilterImpl * object_vs_object_layer_filter = new ObjectLayerPairFilterImpl();
 
 	    this->physics_system.Init(cMaxBodies, cNumBodyMutexes, cMaxBodyPairs, cMaxContactConstraints, *broad_phase_layer_interface, *object_vs_broadphase_layer_filter, *object_vs_object_layer_filter);
+	    this->physics_system.SetBodyActivationListener(&(this->body_activation_listener));
+	    this->physics_system.SetContactListener(&(this->contact_listener));
     }
 
     void update() {
 	    const float cDeltaTime = 1.0f / 60.0f;
+		// If you take larger steps than 1 / 60th of a second you need to do multiple collision steps in order to keep the simulation stable. Do 1 collision step per 1 / 60th of a second (round up).
 		const int cCollisionSteps = 1;
 		this->physics_system.Update(cDeltaTime, cCollisionSteps, &(this->temp_allocator), &(this->job_system));
+    }
+
+    ~Fizyk() {
+	    UnregisterTypes();
+	    delete Factory::sInstance;
+	    Factory::sInstance = nullptr;
     }
 };
 
 
-/* PhysicsSystem * initPhysics() { */
-/* 	PhysicsSystem * physics_system = new PhysicsSystem(); */
-
-/* 	const uint cMaxBodies = 1024; */
-/* 	const uint cNumBodyMutexes = 0; */
-/* 	const uint cMaxBodyPairs = 1024; */
-/* 	const uint cMaxContactConstraints = 1024; */
-/* 	BPLayerInterfaceImpl * broad_phase_layer_interface = new BPLayerInterfaceImpl(); */
-/* 	ObjectVsBroadPhaseLayerFilterImpl * object_vs_broadphase_layer_filter = new ObjectVsBroadPhaseLayerFilterImpl(); */
-/* 	ObjectLayerPairFilterImpl * object_vs_object_layer_filter = new ObjectLayerPairFilterImpl(); */
-
-/* 	physics_system->Init(cMaxBodies, cNumBodyMutexes, cMaxBodyPairs, cMaxContactConstraints, *broad_phase_layer_interface, *object_vs_broadphase_layer_filter, *object_vs_object_layer_filter); */
-
-/*     return physics_system; */
-/* } */
-
-
-
 
 int main(int argc, char** argv) {
-	// Register allocation hook
 	RegisterDefaultAllocator();
-	// Install callbacks
-	Trace = TraceImpl;
-	JPH_IF_ENABLE_ASSERTS(AssertFailed = AssertFailedImpl;)
-	// Create a factory
-	Factory::sInstance = new Factory();
-	RegisterTypes();
-
-	TempAllocatorImpl temp_allocator(10 * 1024 * 1024);
-	JobSystemThreadPool job_system(cMaxPhysicsJobs, cMaxPhysicsBarriers, thread::hardware_concurrency() - 1);
-
-	/* PhysicsSystem * physics_system = initPhysics(); */
-
     Fizyk * f = new Fizyk();
 
-	/* const uint cMaxBodies = 1024; */
-	/* const uint cNumBodyMutexes = 0; */
-	/* const uint cMaxBodyPairs = 1024; */
-	/* const uint cMaxContactConstraints = 1024; */
-	/* BPLayerInterfaceImpl broad_phase_layer_interface; */
-	/* ObjectVsBroadPhaseLayerFilterImpl object_vs_broadphase_layer_filter; */
-	/* ObjectLayerPairFilterImpl object_vs_object_layer_filter; */
-
-	/* physics_system->Init(cMaxBodies, cNumBodyMutexes, cMaxBodyPairs, cMaxContactConstraints, broad_phase_layer_interface, object_vs_broadphase_layer_filter, object_vs_object_layer_filter); */
-	/* PhysicsSystem physics_system; */
-	/* physics_system.Init(cMaxBodies, cNumBodyMutexes, cMaxBodyPairs, cMaxContactConstraints, broad_phase_layer_interface, object_vs_broadphase_layer_filter, object_vs_object_layer_filter); */
-
-	/* PhysicsSystem * physics_system = new PhysicsSystem(); */
-
-	/* PhysicsSystem * ps = initPhysics(); */
-	/* PhysicsSystem physics_system = p[0]; */
-	/* PhysicsSystem physics_system = *initPhysics(); */
-
-
-	MyBodyActivationListener body_activation_listener;
-	/* physics_system.SetBodyActivationListener(&body_activation_listener); */
-	f->physics_system.SetBodyActivationListener(&body_activation_listener);
-	MyContactListener contact_listener;
-	/* physics_system.SetContactListener(&contact_listener); */
-	f->physics_system.SetContactListener(&contact_listener);
-
-
-	/* // The main way to interact with the bodies in the physics system is through the body interface. There is a locking and a non-locking */
-	/* // variant of this. We're going to use the locking version (even though we're not planning to access bodies from multiple threads) */
-	/* BodyInterface &body_interface = physics_system.GetBodyInterface(); */
 	BodyInterface &body_interface = f->physics_system.GetBodyInterface();
 
-	// Next we can create a rigid body to serve as the floor, we make a large box
-	// Create the settings for the collision volume (the shape).
-	// Note that for simple shapes (like boxes) you can also directly construct a BoxShape.
+    // FLOOR
 	BoxShapeSettings floor_shape_settings(Vec3(100.0f, 1.0f, 100.0f));
-
-	// Create the shape
 	ShapeSettings::ShapeResult floor_shape_result = floor_shape_settings.Create();
-	ShapeRefC floor_shape = floor_shape_result.Get(); // We don't expect an error here, but you can check floor_shape_result for HasError() / GetError()
-
-	// Create the settings for the body itself. Note that here you can also set other properties like the restitution / friction.
-	BodyCreationSettings floor_settings(floor_shape, RVec3(0.0_r, -1.0_r, 0.0_r), Quat::sIdentity(), EMotionType::Static, Layers::NON_MOVING);
-
-	// Create the actual rigid body
+	ShapeRefC floor_shape = floor_shape_result.Get();
+    BodyCreationSettings floor_settings(floor_shape, RVec3(0.0_r, -1.0_r, 0.0_r), Quat::sIdentity(), EMotionType::Static, Layers::NON_MOVING);
 	Body *floor = body_interface.CreateBody(floor_settings); // Note that if we run out of bodies this can return nullptr
-
-	// Add it to the world
 	body_interface.AddBody(floor->GetID(), EActivation::DontActivate);
 
-	// Now create a dynamic body to bounce on the floor
-	// Note that this uses the shorthand version of creating and adding a body to the world
+    // BALL
 	BodyCreationSettings sphere_settings(new SphereShape(0.5f), RVec3(0.0_r, 2.0_r, 0.0_r), Quat::sIdentity(), EMotionType::Dynamic, Layers::MOVING);
 	BodyID sphere_id = body_interface.CreateAndAddBody(sphere_settings, EActivation::Activate);
 
-	// Now you can interact with the dynamic body, in this case we're going to give it a velocity.
-	// (note that if we had used CreateBody then we could have set the velocity straight on the body before adding it to the physics system)
 	body_interface.SetLinearVelocity(sphere_id, Vec3(0.0f, -5.0f, 0.0f));
 
-	// We simulate the physics world in discrete time steps. 60 Hz is a good rate to update the physics system.
-	const float cDeltaTime = 1.0f / 60.0f;
-
-	// Optional step: Before starting the physics simulation you can optimize the broad phase. This improves collision detection performance (it's pointless here because we only have 2 bodies).
-	// You should definitely not call this every frame or when e.g. streaming in a new level section as it is an expensive operation.
-	// Instead insert all new objects in batches instead of 1 at a time to keep the broad phase efficient.
-	f->physics_system.OptimizeBroadPhase();
-
-	// Now we're ready to simulate the body, keep simulating until it goes to sleep
 	uint step = 0;
 	while (body_interface.IsActive(sphere_id))
 	{
-		// Next step
 		++step;
-
-		// Output current position and velocity of the sphere
 		RVec3 position = body_interface.GetCenterOfMassPosition(sphere_id);
 		Vec3 velocity = body_interface.GetLinearVelocity(sphere_id);
 		cout << "Step " << step << ": Position = (" << position.GetX() << ", " << position.GetY() << ", " << position.GetZ() << "), Velocity = (" << velocity.GetX() << ", " << velocity.GetY() << ", " << velocity.GetZ() << ")" << endl;
-
-		// If you take larger steps than 1 / 60th of a second you need to do multiple collision steps in order to keep the simulation stable. Do 1 collision step per 1 / 60th of a second (round up).
-		const int cCollisionSteps = 1;
-
-		// Step the world
-		/* f->physics_system.Update(cDeltaTime, cCollisionSteps, &temp_allocator, &job_system); */
 		f->update();
 	}
 
-	// Remove the sphere from the physics system. Note that the sphere itself keeps all of its state and can be re-added at any time.
 	body_interface.RemoveBody(sphere_id);
-
-	// Destroy the sphere. After this the sphere ID is no longer valid.
 	body_interface.DestroyBody(sphere_id);
 
-	// Remove and destroy the floor
 	body_interface.RemoveBody(floor->GetID());
 	body_interface.DestroyBody(floor->GetID());
-
-	// Unregisters all types with the factory and cleans up the default material
-	UnregisterTypes();
-
-	// Destroy the factory
-	delete Factory::sInstance;
-	Factory::sInstance = nullptr;
-
 	return 0;
 }
